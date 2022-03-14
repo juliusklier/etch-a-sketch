2f9261816bf773c81b323bee3dcc79fa52115c85
juliusklier <julius@juliusklier.com>
2022-03-14 08:01:52 +0100

    Added Random Color Button and Eraser function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const container = document.getElementById("container");
 const userInput = document.getElementById("quantity");
-const clearBtn = document.querySelector(".btn");
+const clearBtn = document.querySelector("#clear");
+const randomBtn = document.querySelector("#random");
+const eraseBtn = document.querySelector("#erase")
 
 
 
@@ -42,6 +44,7 @@ changeGrid = () => {
 
 userInput.addEventListener("change", changeGrid);
 
+// clear the grid 
 clearBtn.addEventListener("click", function() {
   container.innerHTML = "";
   userInput.value = "";
@@ -51,9 +54,37 @@ clearBtn.addEventListener("click", function() {
 });
 
 
+// limit the input, so that only positive numbers under 100 are allowed
 function limiter(input) {
   if (input.value < 0) input.value = 0;
   
   if (input.value > 100) input.value = 100;
+}
+
+
+randomBtn.addEventListener("click", randomise);
+eraseBtn.addEventListener("click", erase);
+// changes the background with a random rgb color
+function randomBackground(e) {
+  let randomColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+  e.style.backgroundColor = randomColor;
+}
+
+function whiteBackground(e) {
+  let white = "white";
+  e.style.backgroundColor = white;
+}
+
+// adds eventListener for random rgb color
+function randomise() {
+  document.querySelectorAll(".grid-item").forEach(e => {
+    e.setAttribute("onmouseover", "randomBackground(this)");
+  })
+}
+
+function erase() {
+  document.querySelectorAll(".grid-item").forEach(e => {
+    e.setAttribute("onmouseover","whiteBackground(this)" )
+  })
 }
 makeGrid();
